e7a109256941f232084ad54dc382a4dc08841fc7
NightFlexer <86386178+NightFlexer@users.noreply.github.com>
2021-10-28 12:31:44 +0300

    Add script

---
+++ SQLite get URL.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sqlite3 test.db 'SELECT * FROM test;' | grep "|5" | awk '{ gsub("\|"," "); print $2 }'| sed "s/[/]//g" > url.txt
+com=0 && de=0 && ru=0 && eng=0 && indefined=0
+a=`wc -l < url.txt`
+for ((i=1; i<$a+1; i++))
+do
+b=`sed -n $i"p" url.txt | sed 's/example.//g'`
+  if [[ $b = "com" ]]; then 
+    com=$((com+1))
+  elif [[ $b = "de" ]]; then
+    de=$((de+1))
+  elif [[ $b = "ru" ]]; then
+    ru=$((ru+1))
+  elif [[ $b = "eng" ]]; then
+    eng=$((eng+1))
+  else
+    indefined=$((indefined+1))
+  fi
+done
+echo "Результат: "
+if [[ $com > 0 ]]; then echo "Домен com: "$com; fi
+if [[ $de > 0 ]]; then echo "Домен com: "$de; fi
+if [[ $ru > 0 ]]; then echo "Домен com: "$ru; fi
+if [[ $eng > 0 ]]; then echo "Домен com: "$eng; fi
+if [[ $indefined > 0 ]]; then echo "Домен не определён: "$indefined; fi
